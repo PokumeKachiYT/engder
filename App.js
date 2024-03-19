cbb340dc4f3f778448e9c73f4b9aa38f96fe7260
PokumeKachiYT <pkmcgaming@gmail.com>
2024-03-19 23:04:10 +0700

    gonna submit this shit as a prototype

--- App.js
+++ App.js
@@ -7,14 +7,15 @@ import { StyleSheet, Text, View } from 'react-native'
 import { PanResponder, Animated } from 'react-native'
 import React, { useState, useEffect, useRef } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { LinearGradient } from 'expo-linear-gradient';
+import {LinearGradient} from 'expo-linear-gradient';
 
 let panning = false
 let canPan = false
 let words = null
-const maxRotation = 60
 
-let lastUpdate = Date.now()
+const MAX_ROTATION = 60
+const MAX_PAN = 0.1
+const MAX_GRAD_OPACITY = 0.25
 
 const saveData = async (key, value) => {
   try {
@@ -33,10 +34,28 @@ const getData = async (key) => {
   }
 }
 
+const fetchData = () => {
+  fetch('https://www.mit.edu/~ecprice/wordlist.10000')
+    .then( (res) => res.text() )
+    .then( (data) => {
+      words = data.split('\n')
+            
+      for (let i = 0 ; i < words.length ; ++i) {
+        if (words[i].length <= 2) {
+          words.splice(i,1)
+        }
+      }
+
+      saveData('10000-data',words)
+    } ) 
+}
+
 export default function App() {
   const [rotateY, setRotateY] = useState('0deg')
   const [offsetX, setOffsetX] = useState(0)
-  const [word, setWord] = useState('Swipe if\n< this is a new word\nyou have already known this >')
+  const [gradRed, setGradRed] = useState(0)
+  const [gradGreen, setGradGreen] = useState(0)
+  const [word, setWord] = useState("Swipe if you...\n< don't know this word\nknow this word >")
 
   const pan = useRef(new Animated.ValueXY()).current
   
@@ -55,65 +74,70 @@ export default function App() {
         return true
       },
       onPanResponderMove: (event, gestureState) => {
+        const xValue = pan.x.__getValue()
+        const xRatio = xValue / width
+
         panEvent(event, gestureState)
         setRotateY(
-          Math.max(-maxRotation,Math.min(maxRotation,
-            pan.x.__getValue() * Math.abs(pan.x.__getValue() / 2 / width)
+          Math.max(-MAX_ROTATION,Math.min(MAX_ROTATION,
+            xValue * Math.abs(xValue / 2 / width)
           )) + 'deg'
         )
+
+        if (xRatio >= MAX_PAN) {
+          setGradGreen(Math.min(MAX_GRAD_OPACITY,(xRatio - MAX_PAN)))
+        } else {
+          setGradGreen(0)
+        }
+
+        if (xRatio <= -MAX_PAN) {
+          setGradRed(Math.max(-MAX_GRAD_OPACITY,(MAX_PAN - xRatio) / 2))
+        } else {
+          setGradRed(0)
+        }
       },
 
       onPanResponderRelease: () => {
+        const xValue = pan.x.__getValue()
+        const xRatio = xValue / width
+
         panning = false
         setRotateY('0deg')
-        if (pan.x.__getValue() / width >= 0.15) {
-          setWord(
-            words[~~(Math.random() * words.length)]
-          )
-        }
+        canPan = ~canPan
+        setGradGreen(0)
+        setGradRed(0)
+
         pan.setValue({ x: 0, y: 0 });
+
         pan.extractOffset()
+
+        if (xRatio >= MAX_PAN) {
+          setWord(words[~~(Math.random() * words.length)])
+        }
       },
     }),
   ).current
 
   useEffect( () => {
     if (panning) {
-      //console.log('panning')
-
       setOffsetX(
-        -pan.x.__getValue() * Math.min(Math.abs(pan.x.__getValue() / 1.5 / width),1)
+        -pan.x.__getValue() * Math.min(1,Math.abs(pan.x.__getValue() / 1.5 / width))
       )
     } else {
-      //console.log('nah')
-
       setOffsetX(0)
-
     }
   })
 
 
   useEffect( () => {
     getData('10000-data')
-    .then( (arr) => {words = arr} )
-    
-    if (words === null) {
-      console.log('fetching data')
-      fetch('https://www.mit.edu/~ecprice/wordlist.10000')
-      .then( (res) => {
-        if (res.ok) {
-          return res.text()
-        } else {
-          return 'holy shit bug found'
+      .then( (arr) => {
+        words = arr
+
+        if (words === null) {
+          fetchData()
         }
       } )
-      .then( (data) => {
-        words = data.split('\n')
-        saveData('10000-data',words)
-        console.log('got data!')
-      } )
-      .catch( (error) => console.log(error) )
-    }
   }, [])
 
   const [fontsLoaded] = useFonts({
@@ -128,20 +152,18 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      
       <LinearGradient
-        colors={['rgba(0,1,0,0)', 'transparent']}
-        style={{
-          /*transform: [
-            {translateX: -width},
-          ]*/
-        }}
+        start={{x: 0,y: 0}}
+        end={{x: 1,y: 0}}
+        style={styles.linearGradient}
+        colors={['rgba(255,50,50,' + gradRed + ')','transparent','rgba(50,255,50,' + gradGreen + ')',]}
       />
 
       <Animated.View
         style={{
           transform: [
             {translateX: pan.x.__getValue() + offsetX},
-            //{translateY: pan.y},
           ],
           width: '100%',
           height: '100%',
@@ -149,7 +171,6 @@ export default function App() {
         {...panResponder.panHandlers}>
         
         <Text
-          numberOfLines={1}
           adjustsFontSizeToFit
           style={{
             fontSize: 32,
@@ -177,7 +198,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#9955FF',
-    alignItems: 'center',
+    alignItems: 'initial',
     justifyContent: 'center',
+  },
+  linearGradient: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
   },
 })
